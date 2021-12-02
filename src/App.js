adfab5c30577fa05cdd1ca062376fc7b37f9f67d
bekdev <asadbeksr@gmail.com>
2021-12-03 00:58:29 +0500

    add: done UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import BlogList from "./components/BlogList/BlogList";
+import Menu from "./components/Menu/Menu";
+import History from "./components/History/History";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="app-wrap">
+        <Menu />
+        <History />
+        <BlogList />
+      </div>
+      <Footer />
+    </>
   );
 }
 
