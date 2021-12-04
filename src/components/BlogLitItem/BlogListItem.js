5e42d73fbe55e0815ee8bd50b2486c59e6a4257b
bekdev <asadbeksr@gmail.com>
2021-12-04 16:37:16 +0500

    fix: resposive blog posts

--- src/components/BlogLitItem/BlogListItem.js
+++ src/components/BlogLitItem/BlogListItem.js
@@ -5,20 +5,23 @@ import ViewIcon from "../../assets/view-icon.svg";
 export default function BlogListItem(props) {
   return (
     <div className="single-blog-wrap">
-      <img src={props.src} alt="blog-pic" width="350" height="290" />
-      <div className="blog-time-stats">
-        <span>
-          <p>
-            18:26 11.01.2021 |{" "}
-            <span>
-              <img src={ViewIcon} alt="view-icon" /> 365
-            </span>
-          </p>
-        </span>
+      <img className="single-blog-img" src={props.src} alt="blog-pic" />
+      <div className="single-post-info">
+        <div className="blog-time-stats">
+          <span>
+            <p>
+              18:26 11.01.2021 |{" "}
+              <span>
+                <img src={ViewIcon} alt="view-icon" /> 365
+              </span>
+            </p>
+          </span>
+        </div>
+        <h2 className="single-blog-title">
+          ВОЗ: молодые жители Европы стали меньше курить, но пристрастились
+          .....
+        </h2>
       </div>
-      <h2 className="single-blog-title">
-        ВОЗ: молодые жители Европы стали меньше курить, но пристрастились .....
-      </h2>
     </div>
   );
 }
