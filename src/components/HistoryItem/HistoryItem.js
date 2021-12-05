4e7c7dcb6fa954b14241e5f3dd1c2290377a22f8
bekdev <asadbeksr@gmail.com>
2021-12-05 13:59:37 +0500

    fix: update history items responsiveness

--- src/components/HistoryItem/HistoryItem.js
+++ src/components/HistoryItem/HistoryItem.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./HistoryItem.css";
 export default function HistoryItem(props) {
   return (
-    <div className="single-history">
-      <div className="img-wrap">
-        <img src={props.src} width="100%" height="100%" />
+    <div className="border-wrap">
+      <div className="border-wrap-white">
+        <img className="his-pic" src={props.src} alt="story-img" />
       </div>
     </div>
   );
