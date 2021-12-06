c39880f044d85b261677a9935d1e62083bd63068
bekdev <asadbeksr@gmail.com>
2021-12-07 02:02:58 +0500

    add: lazy loading to img

--- src/components/HistoryItem/HistoryItem.js
+++ src/components/HistoryItem/HistoryItem.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./HistoryItem.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 export default function HistoryItem(props) {
   return (
     <div className="border-wrap">
       <div className="border-wrap-white">
-        <img className="his-pic" src={props.src} alt="story-img" />
+        <LazyLoadImage
+          className="his-pic"
+          src={props.src}
+          alt="story-img"
+          delayTime="1000"
+          effect="blur"
+        />
       </div>
     </div>
   );
