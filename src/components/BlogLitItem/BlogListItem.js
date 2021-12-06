4263551362ab435bd71b0c7ba26371e8b6a1687e
bekdev <asadbeksr@gmail.com>
2021-12-07 01:57:37 +0500

    add: lazy loading to images

--- src/components/BlogLitItem/BlogListItem.js
+++ src/components/BlogLitItem/BlogListItem.js
@@ -2,11 +2,20 @@ import React from "react";
 import "./BlogListItem.css";
 import ViewIcon from "../../assets/view-icon.svg";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 export default function BlogListItem(props) {
   return (
     <div className="single-blog-wrap">
       <Link to={`/post/${props.id}`}>
-        <img className="single-blog-img" src={props.src} alt="blog-pic" />
+        <LazyLoadImage
+          src={props.src}
+          className="single-blog-img"
+          alt="blog-pic"
+          delayTime="1000"
+          effect="blur"
+        />
       </Link>
       <div className="single-post-info">
         <div className="blog-time-stats">
