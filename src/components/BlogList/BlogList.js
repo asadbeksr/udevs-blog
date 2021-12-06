8f0a9d7982cc38332eadb4c1ae57dda690f0dda8
bekdev <asadbeksr@gmail.com>
2021-12-07 01:38:05 +0500

    fix: update link route

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -2,26 +2,31 @@ import React from "react";
 import BlogListItem from "../BlogLitItem/BlogListItem";
 import "./BlogList.css";
 import SingleBlogPic1 from "../../assets/photo-1.png";
-import SingleBlogPic2 from "../../assets/photo-2.png";
-import SingleBlogPic3 from "../../assets/photo-3.png";
-
+// import SingleBlogPic2 from "../../assets/photo-2.png";
+// import SingleBlogPic3 from "../../assets/photo-3.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function BlogList() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(({ data }) => setPosts(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="blog-list">
-      <BlogListItem src={SingleBlogPic1} />
-      <BlogListItem src={SingleBlogPic2} />
-      <BlogListItem src={SingleBlogPic3} />
-      <BlogListItem src={SingleBlogPic1} />
-
-      <BlogListItem src={SingleBlogPic2} />
-      <BlogListItem src={SingleBlogPic3} />
-      <BlogListItem src={SingleBlogPic1} />
-      <BlogListItem src={SingleBlogPic2} />
-
-      <BlogListItem src={SingleBlogPic3} />
-      <BlogListItem src={SingleBlogPic1} />
-      <BlogListItem src={SingleBlogPic2} />
-      <BlogListItem src={SingleBlogPic3} />
+      {posts.map((item, id) => (
+        <BlogListItem
+          src={SingleBlogPic1}
+          PostTitle={item.title}
+          key={item.id}
+          id={item.id}
+        ></BlogListItem>
+      ))}
     </div>
   );
 }
