9fc5fd40861998b78d5932cd788cb44463a94154
bekdev <asadbeksr@gmail.com>
2021-12-07 01:39:25 +0500

    fix: add link route

--- src/components/BlogLitItem/BlogListItem.js
+++ src/components/BlogLitItem/BlogListItem.js
@@ -1,26 +1,29 @@
 import React from "react";
 import "./BlogListItem.css";
 import ViewIcon from "../../assets/view-icon.svg";
-
+import { Link } from "react-router-dom";
 export default function BlogListItem(props) {
   return (
     <div className="single-blog-wrap">
-      <img className="single-blog-img" src={props.src} alt="blog-pic" />
+      <Link to={`/post/${props.id}`}>
+        <img className="single-blog-img" src={props.src} alt="blog-pic" />
+      </Link>
       <div className="single-post-info">
         <div className="blog-time-stats">
           <span>
             <p>
-              18:26 11.01.2021 |{" "}
+              18:26 11.01.2021 |
               <span>
-                <img src={ViewIcon} alt="view-icon" /> 365
+                {(/ /g, "\u00a0")} <img src={ViewIcon} alt="view-icon" />{" "}
+                {(/ /g, "\u00a0")}
+                365
               </span>
             </p>
           </span>
         </div>
-        <h2 className="single-blog-title">
-          ВОЗ: молодые жители Европы стали меньше курить, но пристрастились
-          .....
-        </h2>
+        <Link to={`/post/${props.id}`}>
+          <h2 className="single-blog-title">{props.PostTitle}</h2>
+        </Link>
       </div>
     </div>
   );
