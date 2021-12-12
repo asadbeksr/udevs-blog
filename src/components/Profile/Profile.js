2acec032ee89ec8ea857737202be3e9c83c17dbf
bekdev <asadbeksr@gmail.com>
2021-12-13 00:16:36 +0500

    add: prfoile UI

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Profile.css";
+export default function Profile() {
+  return (
+    <>
+      <div className="profile-info-wrap">
+        <h1>Prfile</h1>
+      </div>
+    </>
+  );
+}
