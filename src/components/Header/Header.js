6d5c1b97a314066578ff7b1b91035ba6184dac78
bekdev <asadbeksr@gmail.com>
2021-12-13 09:20:51 +0500

    fix: update style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,25 +7,6 @@ import { Link } from "react-router-dom";
 import Sign from "../Register/Sign";
 
 export default function Header() {
-  // const [open, setOpen] = useState(false);
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const isAuthorized = localStorage.getItem("isAuthorized");
-
-  // function onSubmit(event) {
-  //   event.preventDefault();
-  //   const key = {
-  //     email: "udevs@gmail.com",
-  //     password: "udevs",
-  //   };
-  //   if (key.email === email && key.password === password) {
-  //     localStorage.setItem("isAuthorized", true);
-  //     setOpen(false);
-  //   } else {
-  //     alert("Incorrect Password or Email");
-  //   }
-  // }
-
   return (
     <div className="header">
       <div className="wrap">
@@ -50,48 +31,7 @@ export default function Header() {
             width="28"
             alt="new-notification-icon"
           />
-          {/* {isAuthorized && (
-            <button
-              onClick={() => {
-                localStorage.removeItem("isAuthorized");
-                window.location.reload(false);
-              }}
-              id="logut-btn"
-            >
-              Выйти
-            </button>
-          )}{" "}
-          {!isAuthorized && (
-            <button onClick={() => setOpen(true)} id="login_btn">
-              Войти
-            </button>
-          )}
-        </div>
-        <div className={`modal ${open ? "visible" : ""}`}>
-          <button id="close" onClick={() => setOpen(false)}>
-            <FontAwesomeIcon icon={faTimes} />
-          </button>
-          <h1>Вход на udevs news</h1>
-          <form onSubmit={onSubmit} autoComplete="off">
-            <input
-              onChange={(event) => setEmail(event.target.value)}
-              type="text"
-              name="email"
-              placeholder="Email"
-            />
-            <input
-              onChange={(event) => setPassword(event.target.value)}
-              type="password"
-              name="password"
-              placeholder="Пароль"
-              autoComplete="on"
-            />
-            <button id="login-btn" type="submit">
-              Войти
-            </button>
-          </form>
-        </div>
-        {open && <div className="overlay" />} */}
+
           <Sign />
         </div>
       </div>
