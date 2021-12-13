86a5ea8b8845eddc5496130141ecd0aef8d942df
bekdev <asadbeksr@gmail.com>
2021-12-13 09:59:33 +0500

    add: publish func

--- src/components/Publish/Publish.js
+++ src/components/Publish/Publish.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Publish.module.css";
+import { db } from "../../firebase";
+import { collection, addDoc } from "@firebase/firestore";
+
 export default function Publish() {
+  const postsCollectionRef = collection(db, "posts");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+
+  const PublishPost = async (e) => {
+    e.preventDefault();
+    await addDoc(postsCollectionRef, {
+      title: title,
+      description: body,
+      image: image,
+    });
+    console.log(postsCollectionRef);
+  };
+
   return (
     <div className={style.publish_wrap}>
       <h1>Настройки публикации </h1>
@@ -9,16 +27,24 @@ export default function Publish() {
         type="text"
         className={style.title_input}
         placeholder="название публикации"
-      ></textarea>
-
+        onChange={(e) => setTitle(e.target.value)}
+      ></textarea>{" "}
+      <h3>Url изображения</h3>
+      <input
+        type="text"
+        className={style.title_input}
+        placeholder="Url изображения"
+        onChange={(e) => setImage(e.target.value)}
+      ></input>
       <h3>Описание</h3>
       <textarea
         type="text"
         className={style.body_input}
         placeholder="расскажите о ..."
+        onChange={(e) => setBody(e.target.value)}
       ></textarea>
       <div>
-        <button className={style.post_btn} type="submit">
+        <button onClick={PublishPost} className={style.post_btn} type="submit">
           Post
         </button>
       </div>
